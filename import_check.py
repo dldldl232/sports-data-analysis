3193f535a3c6e98176bac8c49464dc32ac513658
Seoyeon Kim <seoyeonkim0404@gmail.com>
2025-03-17 17:29:44 -0700

    Initial commit

---
+++ import_check.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn
+import sqlalchemy
+import powerbiclient
+print("✅ All libraries installed successfully!")
